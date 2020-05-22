af7f31814e19b617dee636420f418ff3ea98b015
ghyang-zju <38681094+ghyang-zju@users.noreply.github.com>
2020-05-22 21:02:30 +0800

    Update Get_yhat.m

--- Get_yhat.m
+++ Get_yhat.m
@@ -1,9 +1,8 @@
 function [yhat] = Get_yhat(w,U)
-%UNTITLED6 此处显示有关此函数的摘要
     K = 0.6+w^2;
     T = 3+0.5*w^3;
-    cnum = K; % 分子
-    cden = [T,1]; % 分母
+    cnum = K;
+    cden = [T,1];
     Ts = 1;
     sysc = tf(cnum,cden);
     sysd = c2d(sysc,Ts);

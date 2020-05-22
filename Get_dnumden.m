b5d7dd4c2d3aa72e35a4384e1c065bf385a0b167
ghyang-zju <38681094+ghyang-zju@users.noreply.github.com>
2020-05-22 21:01:59 +0800

    Update Get_dnumden.m

--- Get_dnumden.m
+++ Get_dnumden.m
@@ -1,9 +1,8 @@
 function [dnum,dden] = Get_dnumden(w)
-%UNTITLED2 根据工作点求取离散传函的分子和分母
     K = 0.6+w^2;
     T = 3+0.5*w^3;
-    cnum = K; % 分子
-    cden = [T,1]; % 分母
+    cnum = K; 
+    cden = [T,1];
     Ts = 1;
     sysc = tf(cnum,cden);
     sysd = c2d(sysc,Ts);
